3017c9d32d28704315dd0dff6e4a4be56a3481aa
taha <taha>
2024-02-19 21:06:38 +0500

    first commit

---
+++ CMS/index.php
@@ -0,0 +1,232 @@
+<?php
+
+require_once("config.php");
+if(isset($_POST['email']) && isset($_POST['password'])){
+
+
+$email = $_POST["email"];
+$pass = $_POST["password"];
+//echo $email;
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if(!$con){
+    die("Unable to connect");
+}
+$sql = "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'";
+$res = mysqli_query($con,$sql);
+$count = mysqli_num_rows($res);
+
+
+
+if($count == 1)
+    {
+        header("location:dashboard.php");
+    }
+
+    $message = "Invalid Credentials";
+}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<!-- 
+Template Name:  SmartAdmin Responsive WebApp - Template build with Twitter Bootstrap 4
+Version: 4.4.1
+Author: Sunnyat Ahmmed
+Website: http://gootbootstrap.com
+Purchase: https://wrapbootstrap.com/theme/smartadmin-responsive-webapp-WB0573SK0
+License: You must have a valid license purchased only from wrapbootstrap.com (link above) in order to legally use this theme for your project.
+-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Login - - SmartAdmin v4.4.1
+        </title>
+        <meta name="description" content="Login">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <!-- Call App Mode on ios devices -->
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <!-- Remove Tap Highlight on Windows Phone IE -->
+        <meta name="msapplication-tap-highlight" content="no">
+        <!-- base css -->
+        <link rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <!-- Optional: page related CSS-->
+        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    </head>
+    <body>
+        <div class="page-wrapper">
+            <div class="page-inner bg-brand-gradient">
+                <div class="page-content-wrapper bg-transparent m-0">
+                    <div class="height-10 w-100 shadow-lg px-4 bg-brand-gradient">
+                        <div class="d-flex align-items-center container p-0">
+                            <div class="page-logo width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9">
+                                <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
+                                    
+                                    <p style="font-size: 18px;padding-top: 10px;color:#3995ce; "><b>COMPLAIN MANAGENT SYSTEM</b></p>
+                            </div>
+                            <a href="page_register.php" class="btn-link text-white ml-auto">
+                                Create Account
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex-1" style="background: url(img/svg/pattern-1.svg) no-repeat center bottom fixed; background-size: cover;">
+                        <div class="container py-4 py-lg-5 my-lg-5 px-4 px-sm-0">
+                            <div class="row">
+                                <div class="col col-md-6 col-lg-7 hidden-sm-down">
+                                    <h2 class="fs-xxl fw-500 mt-4 text-white">
+                                        <img src="img/Logo.png	">
+                                        
+                                    </h2>
+                                   	
+                                </div>
+                                <div class="col-sm-12 col-md-6 col-lg-5 col-xl-4 ml-auto">
+                                    <h1 class="text-white fw-300 mb-3 d-sm-block d-md-none">
+                                        Secure login
+                                    </h1>
+                                    <div class="card p-4 rounded-plus bg-faded">
+                                        <form  novalidate="" method="post" 
+                                        action="<?php echo $_SERVER['PHP_SELF'];?>">
+                                            <?php  if(isset($message)){ ?>
+                                            
+
+
+                                            <div class="alert alert-danger">
+                                                <?php
+                                                    echo $message;
+                                                
+                                                ?>
+                                            </div>
+                                                <?php } ?>
+                                            <div class="form-group">
+                                                <label class="form-label" for="username">Username</label>
+                                                <input type="email" id="email" class="form-control form-control-lg" placeholder="your id or email" value="" name="email" required>
+                                                <div class="invalid-feedback">No, you missed this one.</div>
+                                                <div class="help-block">Your unique username to app</div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="form-label" for="password">Password</label>
+                                                <input type="password" id="password" class="form-control form-control-lg" placeholder="password" value="" name="password" required>
+                                                <div class="invalid-feedback">Sorry, you missed this one.</div>
+                                                <div class="help-block">Your password</div>
+                                            </div>
+                                            <div class="form-group text-left">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" id="rememberme">
+                                                    <label class="custom-control-label" for="rememberme"> Remember me for the next 30 days</label>
+                                                </div>
+                                            </div>
+                                            <div class="row no-gutters">
+                                                <div class="col-lg-12 pr-lg-1 my-2">
+                                                    <button type="submit" class="btn btn-info btn-block btn-lg">Login </button>
+                                                </div>
+                                                
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- BEGIN Color profile -->
+        <!-- this area is hidden and will not be seen on screens or screen readers -->
+        <!-- we use this only for CSS color refernce for JS stuff -->
+        <p id="js-color-profile" class="d-none">
+            <span class="color-primary-50"></span>
+            <span class="color-primary-100"></span>
+            <span class="color-primary-200"></span>
+            <span class="color-primary-300"></span>
+            <span class="color-primary-400"></span>
+            <span class="color-primary-500"></span>
+            <span class="color-primary-600"></span>
+            <span class="color-primary-700"></span>
+            <span class="color-primary-800"></span>
+            <span class="color-primary-900"></span>
+            <span class="color-info-50"></span>
+            <span class="color-info-100"></span>
+            <span class="color-info-200"></span>
+            <span class="color-info-300"></span>
+            <span class="color-info-400"></span>
+            <span class="color-info-500"></span>
+            <span class="color-info-600"></span>
+            <span class="color-info-700"></span>
+            <span class="color-info-800"></span>
+            <span class="color-info-900"></span>
+            <span class="color-danger-50"></span>
+            <span class="color-danger-100"></span>
+            <span class="color-danger-200"></span>
+            <span class="color-danger-300"></span>
+            <span class="color-danger-400"></span>
+            <span class="color-danger-500"></span>
+            <span class="color-danger-600"></span>
+            <span class="color-danger-700"></span>
+            <span class="color-danger-800"></span>
+            <span class="color-danger-900"></span>
+            <span class="color-warning-50"></span>
+            <span class="color-warning-100"></span>
+            <span class="color-warning-200"></span>
+            <span class="color-warning-300"></span>
+            <span class="color-warning-400"></span>
+            <span class="color-warning-500"></span>
+            <span class="color-warning-600"></span>
+            <span class="color-warning-700"></span>
+            <span class="color-warning-800"></span>
+            <span class="color-warning-900"></span>
+            <span class="color-success-50"></span>
+            <span class="color-success-100"></span>
+            <span class="color-success-200"></span>
+            <span class="color-success-300"></span>
+            <span class="color-success-400"></span>
+            <span class="color-success-500"></span>
+            <span class="color-success-600"></span>
+            <span class="color-success-700"></span>
+            <span class="color-success-800"></span>
+            <span class="color-success-900"></span>
+            <span class="color-fusion-50"></span>
+            <span class="color-fusion-100"></span>
+            <span class="color-fusion-200"></span>
+            <span class="color-fusion-300"></span>
+            <span class="color-fusion-400"></span>
+            <span class="color-fusion-500"></span>
+            <span class="color-fusion-600"></span>
+            <span class="color-fusion-700"></span>
+            <span class="color-fusion-800"></span>
+            <span class="color-fusion-900"></span>
+        </p>
+        <!-- END Color profile -->
+        <!-- base vendor bundle: 
+			 DOC: if you remove pace.js from core please note on Internet Explorer some CSS animations may execute before a page is fully loaded, resulting 'jump' animations 
+						+ pace.js (recommended)
+						+ jquery.js (core)
+						+ jquery-ui-cust.js (core)
+						+ popper.js (core)
+						+ bootstrap.js (core)
+						+ slimscroll.js (extension)
+						+ app.navigation.js (core)
+						+ ba-throttle-debounce.js (core)
+						+ waves.js (extension)
+						+ smartpanels.js (extension)
+						+ src/../jquery-snippets.js (core) -->
+        
+    </body>
+</html>
